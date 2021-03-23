576fc19218515a9bcb4a179bcc1b4bed42ab6fda
AbbyXSu <xiaotong.s.abby@gmail.com>
2021-03-23 18:50:35 +0000

    test for jenklins build

--- jenkins/test-frontend.sh
+++ jenkins/test-frontend.sh
@@ -3,6 +3,6 @@ git clone https://github.com/AbbyXSu/spring-petclinic-angular.git
 cd spring-petclinic-angular
 sudo apt install -y npm
 npm install -i
-sudo npm test
+sudo npm test --watch false
 cd ..
 sudo rm -rf spring-petclinic-angular
