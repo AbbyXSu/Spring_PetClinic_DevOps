65ce8d9a03845943b6fd0c71fc3c47d87926367e
AbbyXSu <xiaotong.s.abby@gmail.com>
2021-03-23 20:50:50 +0000

    feat: working on jenkins pipelines. Added Maven and JDK innto jenkins global tools configuration also.

--- jenkins/test-frontend.sh
+++ jenkins/test-frontend.sh
@@ -1,6 +1,7 @@
 # Git clone frontend repository
 git clone https://github.com/AbbyXSu/spring-petclinic-angular.git
 cd spring-petclinic-angular
+npm install --save-dev @angular-devkit/build-angular
 npm install -i
 sudo npm test --watch false
 cd ..
