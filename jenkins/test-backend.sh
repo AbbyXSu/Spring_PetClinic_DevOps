5594104d1adfc199479221c1ad5e2ad829e0f42e
Abby X Su <77119427+AbbyXSu@users.noreply.github.com>
2021-03-23 16:43:46 +0000

    Update test-backend.sh

--- jenkins/test-backend.sh
+++ jenkins/test-backend.sh
@@ -1,7 +1,7 @@
 # Git clone from backend repository
 git clone https://github.com/AbbyXSu/spring-petclinic-rest.git
 cd spring-petclinic-rest
-sudo apt install openjdk-8-jre maven default-jre
+sudo apt install openjdk-8-jre
 sudo apt install -y maven
 mvn test
 cd ..
