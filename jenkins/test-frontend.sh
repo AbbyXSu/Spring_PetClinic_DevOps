5aa3f67f9a99022c89ac4167df9a659e619f49c6
Abby X Su <77119427+AbbyXSu@users.noreply.github.com>
2021-03-23 16:58:07 +0000

    Update test-frontend.sh

--- jenkins/test-frontend.sh
+++ jenkins/test-frontend.sh
@@ -2,10 +2,7 @@
 git clone https://github.com/AbbyXSu/spring-petclinic-angular.git
 cd spring-petclinic-angular
 sudo apt install -y npm
-npm install karma
 npm install -i
-sudo npm i karma karma-chrome-launcher chromium
-export CHROME_BIN="/usr/local/lib/node_modules/chromium"
 sudo npm test
 cd ..
 sudo rm -rf spring-petclinic-angular
