baa41ff5858b536d9c2465c2ef4c5ddf585d9d35
AbbyXSu <xiaotong.s.abby@gmail.com>
2021-03-23 21:35:06 +0000

    wip

--- jenkins/test-frontend.sh
+++ jenkins/test-frontend.sh
@@ -1,6 +1,8 @@
 # Git clone frontend repository
+npm install -g @angular/cli@latest
 git clone https://github.com/AbbyXSu/spring-petclinic-angular.git
 cd spring-petclinic-angular
+npm install --save-dev @angular/cli@latest
 npm install --save-dev @angular-devkit/build-angular
 npm install -i
 sudo npm test --watch false
