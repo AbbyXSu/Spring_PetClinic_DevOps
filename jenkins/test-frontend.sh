9d355857792ffc31cf3f56d3e1c88993f1a22c86
Abby X Su <77119427+AbbyXSu@users.noreply.github.com>
2021-03-23 20:06:09 +0000

    Update test-frontend.sh

--- jenkins/test-frontend.sh
+++ jenkins/test-frontend.sh
@@ -1,7 +1,6 @@
 # Git clone frontend repository
 git clone https://github.com/AbbyXSu/spring-petclinic-angular.git
 cd spring-petclinic-angular
-npm install -i
 sudo npm test --watch false
 cd ..
 sudo rm -rf spring-petclinic-angular
